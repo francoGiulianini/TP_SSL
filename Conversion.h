b69def339e4af3becf1435e69590a88a9c8d096f
matiasblanca <32916242+matiasblanca@users.noreply.github.com>
2018-05-27 17:52:51 -0300

    Update and rename conversion.h to Conversion.h
    Saqué las declaraciones de las variables constantes y las puse en TablasDeConversion,c

--- Conversion.h
+++ Conversion.h
@@ -1,11 +1,5 @@
 #ifndef CONVERSION_H_
 #define CONVERSION_H_
-#include <stdlib.h>
-#include <stdio.h>
-
-const int UPPER = 300;
-const int STEP = 20;
-const int LOWER = 0;
 
 double convertirACelsius(double fahr);
 double convertirAFahr(double celsius);
