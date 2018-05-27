298e34106e7a4d41cb68a5e6000ffd40af581261
matiasblanca <32916242+matiasblanca@users.noreply.github.com>
2018-05-27 17:44:49 -0300

    Agrego TablasDeConversion.c

---
+++ TablasDeConversion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "conversion.h"
+
+int main(void){
+	
+	const int UPPER = 300;
+	const int STEP = 20;
+	const int LOWER = 0;
+ 	
+	for(int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+ 	printf("%3d %6.1f\n", fahr, convertirACelsius(fahr));
+
+	for(int cels = LOWER; cels <= UPPER; cels = cels + STEP)
+ 	printf("%3d %6.1f\n", cels, convertirAFahr(cels));
+
+}
