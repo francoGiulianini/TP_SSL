14ba9d050686a8c19ff7ac7a8ba32e89a9cad3ed
matiasblanca <32916242+matiasblanca@users.noreply.github.com>
2018-05-30 22:32:51 -0300

    Update TablasDeConversion.c

--- TablasDeConversion.c
+++ TablasDeConversion.c
@@ -1,17 +1,39 @@
 #include "Conversion.h"
 
-int main(void){
+void printFila(double num1, double num2) {
+	printf("%3.0f %6.1f\n", num1, num2);
+}
+
+void printFilas(int lower, int upper, int step, double (* ptr)(double)) {
+	for(int i = lower; i <= upper; i = i + step)
+ 		printFila(i, (* ptr)(i));
+}
+
+void printTablaCelsius(int lower, int upper, int step) {
+	double (* fptr)(double);
+	fptr = convertirAFahr;
+	printFilas(lower, upper, step, fptr);
+}
+
+void printTablaFahr(int lower, int upper, int step) {
+	double (* fptr)(double);
+	fptr = convertirACelsius;
+	printFilas(lower, upper, step, fptr);;
+}
+
+void printTablas(int lower, int upper, int step) {
+	printTablaCelsius(lower, upper, step);
+	printTablaFahr(lower, upper, step);
+}
+
+
+int main(void) {
 
 	const int UPPER = 300;
 	const int STEP = 20;
 	const int LOWER = 0;
-
-	for(int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
- 		printf("%3d %6.1f\n", fahr, convertirACelsius(fahr));
-
-	for(int cels = LOWER; cels <= UPPER; cels = cels + STEP)
- 		printf("%3d %6.1f\n", cels, convertirAFahr(cels));
-
+	
+	printTablas(LOWER, UPPER, STEP);
+	
 	return EXIT_SUCCESS;
-
 }
